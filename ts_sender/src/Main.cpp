00f83a69a48d845760db8b4fafaf090e832df41c
azeroc <azeroc2601@gmail.com>
2017-04-21 14:15:22 +0300

    Added boost libs

--- ts_sender/src/Main.cpp
+++ ts_sender/src/Main.cpp
@@ -1,8 +1,18 @@
+#include <boost/lambda/lambda.hpp>
 #include <iostream>
+#include <iterator>
+#include <algorithm>
 #include "Constants.h"
 
 int main(int argc, char* argv[])
 {
+	using namespace boost::lambda;
+	typedef std::istream_iterator<int> in;
+
     std::cout << "ts_sender test, const value: " << TS_SENDER_CONST << std::endl;
+
+	std::for_each(
+		in(std::cin), in(), std::cout << (_1 * 3) << " ");
+
     return 0;
 }
